defd5f503e9c96f7bc53230e813a073d7a910355
hyy <2645174806hy@gmail.com>
2026-03-30 16:29:04 +0800

    chore:安装、配置better-auth

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { auth } from './lib/auth';
+import { cors } from 'hono/cors';
+// import { route, handler } from './api/v1/get-allbooks';
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type'],
+    credentials: true,
+  }),
+);
 
-export default app
+app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw));
+
+// app.openapi(route, handler);
+
+export default {
+  port: 3001,
+  fetch: app.fetch,
+};
