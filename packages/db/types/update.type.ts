18ad869d401e3fe7fb75cf01563b9b985d14f247
hyy <2645174806hy@gmail.com>
2026-04-02 23:01:17 +0800

    fix:修改类型

--- packages/db/types/update.type.ts
+++ packages/db/types/update.type.ts
@@ -1,14 +1,15 @@
 import { z } from 'zod';
 
 export const updateBookSchema = z.object({
-  title: z.string(),
-  author: z.string(),
-  publisher: z.string(),
-  publicationDate: z.coerce.date(),
-  categoryId: z.string(),
-  price: z.number(),
-  total: z.number(),
-  available: z.number(),
-  status: z.enum(['在馆', '借出', '遗失', '损坏']).nullable(),
-
+  title: z.string().optional(),
+  author: z.string().optional(),
+  publisher: z.string().optional(),
+  publicationDate: z.coerce.date().optional(),
+  categoryId: z.string().optional(),
+  price: z.number().optional(),
+  total: z.number().optional(),
+  available: z.number().optional(),
+  status: z.enum(['在馆', '借出', '遗失', '损坏']).nullable().optional(),
 });
+
+export type UpdateBookRequest = z.infer<typeof updateBookSchema>;
