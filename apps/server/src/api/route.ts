8917f6d56dae320eb2492ed186fb58ab94c79aa7
hyy <2645174806hy@gmail.com>
2026-04-07 16:11:22 +0800

    fix:改变暴露路由方式

--- apps/server/src/api/route.ts
+++ apps/server/src/api/route.ts
@@ -1,13 +1,16 @@
-import type { OpenAPIHono } from '@hono/zod-openapi';
+import { OpenAPIHono } from '@hono/zod-openapi';
 import { applicationApp } from './v1/application.handler';
 import { bookApp } from './v1/book.handler';
 
+export const apiRoutes = new OpenAPIHono()
+.route('/',bookApp)
+.route('/',applicationApp)
+
 export function initRoutes(app: OpenAPIHono) {
   // 注册路由
-  app.route('/api/v1', bookApp);
-  app.route('/api/v1', applicationApp);
+  app.route('/api/v1', apiRoutes);
 }
 
-export type BookType = typeof bookApp;
-export type ApplicationAppType = typeof applicationApp;
-export type AppType = BookType & ApplicationAppType;
+export type AppType = typeof apiRoutes;
+// export type ApplicationAppType = typeof applicationApp;
+// export type AppType = BookType & ApplicationAppType;
