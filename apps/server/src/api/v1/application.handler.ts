c8800c7cf20366a4380b6a9c5c9465c615dc9b30
hyy <2645174806hy@gmail.com>
2026-04-08 11:33:05 +0800

    feat:新增借阅申请图书接口

--- apps/server/src/api/v1/application.handler.ts
+++ apps/server/src/api/v1/application.handler.ts
@@ -1,10 +1,14 @@
 import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
 import { applicationSchema } from "@repo/types/application.type";
+import { applicationDialogSchema} from "@repo/types/application-dialog.type";
 import { db } from "@repo/db";
 import { borrowApplications } from "@repo/db/schema/borrow-schema";
 import { books } from "@repo/db/schema/book-schema";
 import { eq } from "drizzle-orm";
 import { user } from "@repo/db/schema/auth-schema";
+import { getSession } from "server/src/lib/get-session";
+import dayjs from "dayjs";
+
 
 const app = new OpenAPIHono();
 // 获取全部申请信息
@@ -23,6 +27,51 @@ export const listApplicationRoute = createRoute({
     },
   },
 });
+// 新增申请信息
+export const createApplicationRoute = createRoute({
+  method: 'post',
+  path: '/applications',
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: applicationDialogSchema,
+        },
+      },
+      description: '新增申请信息',
+    },
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: applicationDialogSchema,
+        },
+      },
+      description: '新增申请信息',
+    },
+    401: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+      description: '未登录',
+    },
+    400: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+      description: '参数错误',
+    },
+  },
+});
 export const applicationApp=app
 .openapi(listApplicationRoute, async(c) => {
     const applications = await db
@@ -38,6 +87,36 @@ export const applicationApp=app
         };
       });
   return c.json(result,200);
+})
+
+.openapi(createApplicationRoute, async(c) => {
+  const session = await getSession(c.req.raw.headers);
+  if (!session) {
+    return c.json({ message: '未登录' }, 401);
+  }
+  const body = await c.req.json();
+  const application = applicationDialogSchema.parse(body);
+ const borrowDate= application.borrowDate;
+ const returnDate= application.returnDate;
+ const bookId=application.bookId;
+ if(borrowDate>=returnDate){
+  return c.json({ message: '借阅日期不能早于归还日期' }, 400);
+ }
+ //  获取可用数量
+const [book] = await db.select().from(books).where(eq(books.id, bookId));
+ if(!book){
+  return c.json({ message: '书籍不存在' }, 400);
+ }
+ if(book.available<=0){
+  return c.json({ message: '书籍不可借' }, 400);
+ }
+  const [applications] = await db.insert(borrowApplications).values({
+    ...application,
+    status: '待审核',
+    createdAt: dayjs().unix(), 
+    updatedAt: dayjs().unix(),
+  }).returning();
+  return c.json(applications,200);
 });
 
 export type ApplicationAppType = typeof applicationApp;
---
+++ packages/types/application-dialog.type.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const applicationDialogSchema = z.object({
+  userId: z.string(),
+  bookId: z.number(),
+  borrowDate: z.number(),
+  returnDate: z.number(),
+  status: z.enum(['待审核', '已批准', '已拒绝', '已取消']).nullable(),
+});
+export type ApplicationDialogSchema = z.infer<typeof applicationDialogSchema>;
