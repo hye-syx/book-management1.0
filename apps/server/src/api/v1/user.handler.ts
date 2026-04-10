903a172ba4784af5405afd16e6aa73f814801182
hyy <2645174806hy@gmail.com>
2026-04-10 15:13:06 +0800

    feat:新增删除用户接口

--- apps/server/src/api/v1/user.handler.ts
+++ apps/server/src/api/v1/user.handler.ts
@@ -129,8 +129,48 @@ export const updateUserRoute = createRoute({
     },
   },
 });
-
 // 删除用户信息
+export const deleteUserRoute=createRoute({
+  method: 'delete',
+  path: '/user/{id}',
+  request: {
+    params: z.object({
+      id: z.string(),
+    }),
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+      description: '删除图书',
+    },
+    401: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+      description: '未登录',
+    },
+    403:{
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+      description: '没有权限',
+    }
+  },
+});
 
 export const userApp = app
   .openapi(listUserRoute, async (c) => {
@@ -176,6 +216,18 @@ export const userApp = app
     const { id } = c.req.param();
     const [userResult] = await db.select().from(user).where(eq(user.id, id));
     return c.json(userResult, 200);
+  })
+  .openapi(deleteUserRoute, async (c) => {
+    const session = await getSession(c.req.raw.headers);
+    if (!session) {
+      return c.json({ message: '未登录' }, 401);
+    }
+    if (session.user.role !== 'admin') {
+      return c.json({ message: '权限不足' }, 403);
+    }
+    const { id } = c.req.param();
+    await db.delete(user).where(eq(user.id, id));
+    return c.json({message:'删除成功'}, 200);
   });
 
 export type UserAppType = typeof userApp;
